e6d511402b27ff944f8e6f29d7bd9fddcead1a39
Jiao Lin <linjiao@caltech.edu>
2007-10-11 00:14:19 +0000

    branch "ARCS-eventmodedata-histogrammer": added ipix, a small tool to get I(pix) (and I(det), I(pack) ) out of event data
    git-svn-id: svn://danse.us/histogram/branches/ARCS-eventmodedata-histogrammer@1313 566a22ba-31b3-4a2e-9087-23d4df278cd3

--- lib/ipix.cc
+++ lib/ipix.cc
@@ -4,7 +4,7 @@
 #include "readevent.h"
 #include "histogrammer.h"
 
-int run( const char *filename,  size_t Nevents,  unsigned int tofbegin, unsigned int tofend, unsigned int tofstep,
+int run( const char *filename,  size_t Nevents,  unsigned int pixbegin, unsigned int pixend, unsigned int pixstep,
 	 const char *outfilename)
 {
 
@@ -16,7 +16,7 @@ int run( const char *filename,  size_t Nevents,  unsigned int tofbegin, unsigned
   Event * pevents = reader.read( Nevents );
 
 
-  unsigned int nbins = (tofend-tofbegin)/ tofstep;
+  unsigned int nbins = (pixend-pixbegin)/ pixstep;
 
   unsigned int counts [ nbins ];
 
@@ -31,13 +31,13 @@ int run( const char *filename,  size_t Nevents,  unsigned int tofbegin, unsigned
   unsigned int indexes[1];
 
   for (int i=0; i<Nevents; i++) {
-    const uint &tof = pevents[i].tof;
+    const uint &pix = pevents[i].pixelID;
     
-    if (tof < tofbegin || tof > tofend) {
-      std::cout << "tof " << tof << " is out of bound" << std::endl;
+    if (pix < pixbegin || pix > pixend) {
+      std::cout << "pix " << pix << " is out of bound" << std::endl;
       continue;
     }
-    indexes[0] = (tof - tofbegin)/tofstep;
+    indexes[0] = (pix - pixbegin)/pixstep;
 
     her.increment( indexes );
 
@@ -48,7 +48,7 @@ int run( const char *filename,  size_t Nevents,  unsigned int tofbegin, unsigned
   std::ofstream of( outfilename );
 
   for (int i=0; i<nbins; i++) {
-    of << tofbegin + i * tofstep + tofstep/2 << "\t" << counts[i] << std::endl;
+    of << pixbegin + i * pixstep + pixstep/2 << "\t" << counts[i] << std::endl;
   }
 
   of.close();
@@ -65,13 +65,13 @@ int main( int argc, char ** argv )
 
   size_t Nevents = atoi( argv[2] );
   
-  unsigned int tofbegin = atoi( argv[3] );
-  unsigned int tofend = atoi( argv[4] );
-  unsigned int tofstep = atoi( argv[5] );
+  unsigned int pixbegin = atoi( argv[3] );
+  unsigned int pixend = atoi( argv[4] );
+  unsigned int pixstep = atoi( argv[5] );
 
   char *ofilename = argv[6];
 
-  run( filename, Nevents, tofbegin, tofend, tofstep, ofilename );
+  run( filename, Nevents, pixbegin, pixend, pixstep, ofilename );
 
   return 0;
 }
