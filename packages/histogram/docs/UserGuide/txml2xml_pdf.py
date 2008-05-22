316639b4b70a0d5ce36a416c77c046318fba7296
Jiao Lin <linjiao@caltech.edu>
2008-05-22 08:51:34 +0000

    typo
    git-svn-id: svn://danse.us/histogram/releases/DRCS-1.2@1387 566a22ba-31b3-4a2e-9087-23d4df278cd3

--- packages/histogram/docs/UserGuide/txml2xml_pdf.py
+++ packages/histogram/docs/UserGuide/txml2xml_pdf.py
@@ -5,7 +5,9 @@
 # web_absolutepath is for pdf format product. Because pdf cannot contain movies and some other medias, we have to used absolute web links
 # web_relativepath is for html tar ball producet. It can contain everything that is documentation in one tar ball, so in most cases relative link is sufficient. Certainly there still are cases where you want to put up absolute links, for example those links to download binary installers, which should not be included in the documentation tar ball..
 
+from webserver import webserver
 
 urldict = {
-    'xxxWEBSITExxx': "http://arcscluster.caltech.edu:5001/Docs/docs",
+    'xxxWEBSITExxx': "%s/Docs/docs" % webserver,
+    'xxxDOWNLOADSxxx': "%s/click_monitor" % webserver,
     }
